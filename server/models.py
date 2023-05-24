b1da336d1c7bc2f4d47b1a82172eea015f851e2c
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-24 16:21:56 -0600

    confused

--- server/models.py
+++ server/models.py
@@ -12,6 +12,18 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("You must enter a name!")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, number):
+        if len(number) != 10:
+            raise ValueError("Phone number must be exactly 10 digits!")
+        return number
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +39,29 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise IntegrityError("You must have a title!")
+        return title
+    
+    @validates('content')
+    def validate_post_content(self, key, text):
+        if len(text) < 250:
+            raise ValueError("Post content must be at least 250 characters long!")
+        return text
+    
+    @validates('summary')
+    def validate_post_summary(self, key, text):
+        if len(text) > 250:
+            raise ValueError("Post summary must be 250 characters or less!")
+        return text
+    
+    @validates('category')
+    def validate_category(self, key, cat):
+        if cat != 'Fiction' or cat != 'Non-Fiction':
+            raise ValueError("Category must be 'Fiction' or 'Non-Fiction'")
+        return cat
+    
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
