e1951e67ec9ae42e8a86ba3e72475fe179173046
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-24 16:26:08 -0600

    final commit;

--- server/models.py
+++ server/models.py
@@ -4,7 +4,6 @@ db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
@@ -12,24 +11,26 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    @validates('name')
-    def validate_name(self, key, name):
-        if not name:
-            raise ValueError("You must enter a name!")
-        return name
-    
-    @validates('phone_number')
-    def validate_phone_number(self, key, number):
-        if len(number) != 10:
-            raise ValueError("Phone number must be exactly 10 digits!")
-        return number
-    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
+    @validates('name')
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if not name:
+            raise ValueError("Name field is required.")
+        elif name in names:
+            raise ValueError("Name must be unique.")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
+
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -41,27 +42,24 @@ class Post(db.Model):
 
     @validates('title')
     def validate_title(self, key, title):
-        if not title:
-            raise IntegrityError("You must have a title!")
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
         return title
-    
-    @validates('content')
-    def validate_post_content(self, key, text):
-        if len(text) < 250:
-            raise ValueError("Post content must be at least 250 characters long!")
-        return text
-    
-    @validates('summary')
-    def validate_post_summary(self, key, text):
-        if len(text) > 250:
-            raise ValueError("Post summary must be 250 characters or less!")
-        return text
-    
+
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if(key == 'content') and len(string) < 250:
+            raise ValueError("Post content must be greater than or equal 250 characters long.")
+        elif(key == 'summary') and len(string) >= 250:
+            raise ValueError("Post summary must be less than or equal to 250 characters long.")
+        return string
+
     @validates('category')
-    def validate_category(self, key, cat):
-        if cat != 'Fiction' or cat != 'Non-Fiction':
-            raise ValueError("Category must be 'Fiction' or 'Non-Fiction'")
-        return cat
-    
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError("Category must be Fiction or Non-Fiction.")
+        return category
+
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
